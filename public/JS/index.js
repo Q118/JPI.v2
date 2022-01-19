25633c36ad9cc864717f65ef4e49730815bf246e
Shelby Rothman <srothman@Sroth-7PZB163.corelation.local>
2022-01-19 11:49:00 -0800

    sendign req from public to server success

--- public/JS/index.js
+++ public/JS/index.js
@@ -13,92 +13,31 @@ $(() => {
     const classSelect = $("select#class-select");
     const priorAttendance = $("input#priorAttendance-select");
 
-    const personInfo = [nameInput, emailInput, phoneInput, cuInput, classSelect];
+    const personInfo = [nameInput, emailInput, phoneInput, cuInput, classSelect, priorAttendance];
 
-    // let dataObj;
-    //on submit of form
-    // questionForm.on("submit", function (event) {
-    //     //prevent the default behavior of the form
-    //     event.preventDefault();
-    //     // get the value of the selected
-    //     // const selectedClass = classSelect.val();
-    //     console.log(classSelect.val());
-
-    //     // if (nameInput.val() != "") {
-    //     // if any of the elements from personINfo are empty, alert the user
-    //     if (personInfo.some(el => el.val() === "") || personInfo.some(el => el.val() === "select. . .")) {
-    //         //run the function
-    //         alert("please fill out all the required fields!")
-    //     } else {
-    //         // dataObj = {
-    //         //     "fields": {
-    //         //         "project":
-    //         //         {
-    //         //             "key": "VCRT"
-    //         //         },
-    //         //         "summary": `${nameInput.val()} -- ${classSelect.val()}`,
-    //         //         "description": `CU: ${cuInput.val()}}, Phone: ${phoneInput.val()}, Email: ${emailInput.val()}`,
-    //         //         "issuetype": {
-    //         //             "name": "Basic Functionality"
-    //         //         }
-    //         //     }
-    //         // }
-
-    //         // personInfo.forEach(el => sendData(el.val()));
-    //         $.ajax
-    //             ({
-    //                 ReferrerPolicy: 'no-referrer',
-    //                 type: "POST",
-    //                 contentType: "application/json",
-    //                 url: "/api",
-    //                 data: {
-    //                     "name": "shelby",
-    //                     "email": "Some Email"
-    //                 },
-    //                 success: () => {
-    //                     console.log('Sent to server-side');
-    //                 }
-    //             });
-    //         // use fetch to send form data to /api
-    //         // fetch.ReferrerPolicy = 'no-referrer';
-    //         //             fetch('/api', {
-    //         //                 method: 'POST',
-    //         //                 headers: {
-    //         //                     'Content-Type': 'application/json'
-    //         //                 },
-    //         //                 data: {
-    //         //                     'name': nameInput.val(),
-    //         //                     'email': emailInput.val(),
-    //         //                     'phone': phoneInput.val(),
-    //         //                     'cu': cuInput.val(),
-    //         //                     'class': classSelect.val(),
-    //         //                     'priorAttendance': priorAttendance.val()
-    //         //                 },
-    //         //                 // ReferrerPolicy: "strict-origin-when-cross-origin",
-    //         //             }).then(response => {
-    //         //                 console.log(response);
-    //         //             }).catch(error => console.error('Error:', error))
-
-
-    //     }
-    // });
 
     questionForm.on("submit", function (event) {
         event.preventDefault();
-                    $.ajax
-                ({
-                    Headers: { 'ReferrerPolicy': 'Access-Control-Allow-Origin' },
-                    type: "POST",
-                    contentType: "application/json",
-                    url: "/postTicket",
-                    data: {
-                        "name": "shelby",
-                        "email": "Some Email"
-                    },
-                    success: () => {
-                        console.log('Sent to server-side');
-                    }
-                });
+        if (personInfo.some(el => el.val() === "") || personInfo.some(el => el.val() === "select. . .")) {
+            alert("please fill out all the required fields!")
+        } else {
+            $.ajax({
+                type: "POST",
+                contentType: "application/x-www-form-urlencoded",
+                url: "/postTicket",
+                data: {
+                    name: nameInput.val(),
+                    email: emailInput.val(),
+                    phone: phoneInput.val(),
+                    cu: cuInput.val(),
+                    class: classSelect.val(),
+                    priorAttendance: priorAttendance.val()
+                },
+                success: () => {
+                    console.log('Sent to server-side');
+                }
+            });
+        }
     })
 
 
--- server.js
+++ server.js
@@ -1,12 +1,79 @@
+
 const express = require('express');
 const path = require('path');
 const app = express();
 const port = 8020;
 const cors = require('cors');
+// const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 const fetch = require('node-fetch');
+// import { fetch } from 'node-fetch';
 
-app.use(express.json({ limit: '20mb' }));
-app.use(express.urlencoded({ extended: false, limit: '20mb' }));
+// Serve static content for the app from the "public" directory 
 app.use(express.static("public"));
-// app.use('/', router);
+// Parse application body
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
 app.use(cors());
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+app.get('/postTicket', (req, res) => {
+    console.log(req.body);
+})
+
+app.post('/postTicket', (req, res) => {
+    console.log(req.body.name)
+    const name = req.body.name;
+    const email = req.body.email;
+    const phone = req.body.phone;
+    const cu = req.body.cu;
+    const classSelect = req.body.class;
+    const priorAttendance = req.body.priorAttendance;
+
+console.log(`name: ${name}`);
+console.log(`email: ${email}`);
+console.log(`phone: ${phone}`);
+console.log(`cu: ${cu}`);
+console.log(`class: ${classSelect}`);
+
+    fetch('http://jira.corelationinc.com/rest/api/2/issue/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Basic c3JvdGhtYW46UmF0dDNhdHQh',
+        },
+        body: {
+            "fields": {
+                "project":
+                {
+                    "key": "VCRT"
+                },
+                "summary": `${name} -- ${classSelect}`,
+                "description": `CU: ${cu},
+                                Phone: ${phone}, 
+                                Email: ${email},
+                                Prior Attendance?: ${priorAttendance}`,
+                "issuetype": {
+                    "name": "Basic Functionality"
+                }
+            }
+        }
+    })
+        .then(response => {
+            res.send(response.json());
+            console.log(response);
+        })
+        .catch(error => console.error('Error:', error))
+
+})
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
