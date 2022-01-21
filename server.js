91ee91c05cb95d175bedb32955ca30685ba3d09a
Shelby Rothman <srothman@Sroth-7PZB163.corelation.local>
2022-01-21 13:46:02 -0800

    logic implemented for updateTicket()

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const path = require('path');
 const app = express();
@@ -12,9 +11,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-//todo: change logic in postTicket route to have title be the title of the class (+only creating ticket if one not exist already) and description hold the name and info in a row of a table
-// if ticket exists, then update ticket with new info
-//todo add in  description to 'see comments for registers details'
+
+//todo add in  description to 'see comments for registers details' 
+//todo also add the 'IP' and 'Notes' in the main description
 
 
 app.use(cors());
@@ -22,6 +21,62 @@ app.use(cors());
 app.get("/", (req, res) => {
     res.render("index");
 });
+
+//variable to hold the description of the ticket that exists
+// we need this to save and then append to it the next section of info
+let ticketDescription;
+let issueName; //also need the title for the same reason
+
+let ticketExistence; //boolean to keep track of existence of ticket
+const checkTicketExistence = (summary) => {
+    return new Promise((resolve, reject) => {
+        ticketExistence = false;
+        let currentList;
+        //todo: must change projectName to TT once in production!!!
+        const bodyData = `{ "jql": "project = VCRT AND status=Open AND resolution = Unresolved",
+                            "maxResults": 100,
+                            "fields": [
+                                "summary"
+                            ],
+                            "startAt": 0
+                            }`;
+        fetch('http://jira.corelationinc.com/rest/api/2/search/', {
+            method: 'POST',
+            headers: {
+                'Authorization': 'Basic c3JvdGhtYW46UmF0dDNhdHQh',
+                'Accept': 'application/json, text/plain, */*',
+                'Content-Type': 'application/json'
+            },
+            body: bodyData
+        }).then(response => {
+            console.log(
+                `Response: ${response.status} ${response.statusText}`
+            );
+            return response.text();
+        }).then(text => {
+            console.log("type of: " + typeof text)
+            currentList = JSON.parse(text);
+            let ticketList = []; // push every element from currentList.issues.summary into an array
+            for (let i = 0; i < currentList.issues.length; i++) {
+                ticketList.push(currentList.issues[i].fields.summary);
+                ticketDescription = currentList.issues[i].fields.description;
+                issueName = currentList.issues[i].key;
+            }
+            // forEach element in ticketList, check if the summary matches the summary of the ticket user wants to create
+            ticketList.forEach(element => {
+                console.log(element)
+                if (element === summary) {
+                    console.log("ticket exists in scope of function!");
+                    ticketExistence = true;
+                }
+            });
+            resolve();
+        }).catch(err => {
+            console.error(err)
+            reject();
+        });
+    });
+}
 
 //function to send ticket if it does not exist
 const sendTicket = (classSelect, attendeeName, cu, phone, email, priorAttendance) => {
@@ -61,58 +116,52 @@ const sendTicket = (classSelect, attendeeName, cu, phone, email, priorAttendance
         })
     });
 }
-//function to send if it does exist
-const updateTicket = (summary, description) => {
-}
 
-let ticketExistence;
-const checkTicketExistence = (summary) => {
+//todo: function to add comment to a newly created ticket that will run right after the above one. to notify the trainer and put the additonal info in
+
+
+//function to send if it does exist
+const updateTicket = (classSelect, attendeeName, cu, phone, email, priorAttendance) => {
     return new Promise((resolve, reject) => {
-        ticketExistence = false;
-        let currentList;
-        const bodyData = `{ "jql": "project = VCRT AND status=Open AND resolution = Unresolved",
-                            "maxResults": 100,
-                            "fields": [
-                                "summary"
-                            ],
-                            "startAt": 0
-                            }`;
-        fetch('http://jira.corelationinc.com/rest/api/2/search/', {
-            method: 'POST',
+        fetch(`http://jira.corelationinc.com/rest/api/2/issue/${issueName}`, {
+            method: 'PUT',
             headers: {
-                'Authorization': 'Basic c3JvdGhtYW46UmF0dDNhdHQh',
                 'Accept': 'application/json, text/plain, */*',
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                'Authorization': 'Basic c3JvdGhtYW46UmF0dDNhdHQh',
             },
-            body: bodyData
-        }).then(response => {
-            console.log(
-                `Response: ${response.status} ${response.statusText}`
-            );
-            return response.text();
-        }).then(text => {
-            console.log("type of: " + typeof text)
-            currentList = JSON.parse(text);
-            let ticketList = []; // push every element from currentList.issues.summary into an array
-            for (let i = 0; i < currentList.issues.length; i++) {
-                ticketList.push(currentList.issues[i].fields.summary);
-            }
-            // forEach element in ticketList, check if the summary matches the summary of the ticket user wants to create
-            ticketList.forEach(element => {
-                console.log(element)
-                if (element === summary) {
-                    console.log("ticket exists in scope of function!");
-                    ticketExistence = true;
+            body: JSON.stringify({
+                "update": {
+                    "description": [
+                        {
+                            "set": `${ticketDescription}
+                                    Name: ${attendeeName}
+                                    CU: ${cu},
+                                    Phone: ${phone},
+                                    Email: ${email},
+                                    Prior Attendance?: ${priorAttendance}
+                                    ----`
+                        }
+                    ],
+                    "comment": [
+                        {
+                            "add": {
+                                "body": "This comment will hold additional info about the new attendee and it will tag a trainer to notify them of a new registration."
+                            }
+                        }
+                    ]
                 }
-            });
+            })
+        }).then(response => {
+            res.send(response.json());
+            console.log(response);
             resolve();
-        }).catch(err => {
-            console.error(err)
+        }).catch(error => {
+            console.error('Error:', error)
             reject();
-        });
+        })
     });
 }
-
 
 app.get('/postTicket', async (req, res) => {
     console.log(req.body);
@@ -138,7 +187,7 @@ app.post('/postTicket', async (req, res) => {
         console.log("ticket does not exist");
         await sendTicket(classSelect, attendeeName, cu, phone, email, priorAttendance).catch(err => console.error(err));
     }
-   // fetch('http://jira.corelationinc.com/rest/api/2/issue/', {
+    // fetch('http://jira.corelationinc.com/rest/api/2/issue/', {
     //     method: 'POST',
     //     headers: {
     //         'Accept': 'application/json, text/plain, */*',
